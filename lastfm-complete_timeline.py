b3111f4973a6b73703a9e3ad3afe9b3839382d6e
dbeley <6568955+dbeley@users.noreply.github.com>
2019-07-11 16:18:41 +0200

    Update lastfm-complete-timeline.py

--- lastfm-complete_timeline.py
+++ lastfm-complete_timeline.py
@@ -37,11 +37,13 @@ def main():
         exit()
 
     if args.file:
+        logger.info("Reading %s.", args.file)
         df_initial = pd.read_csv(args.file, sep="\t")
         logger.debug("Columns : %s", list(df_initial))
         max_timestamp = df_initial["Timestamp"].astype(int).max()
         logger.debug("Maximum timestamp = %s", max_timestamp)
         # for small updates, limit=None works fine
+        logger.info("Extracting scrobbles since %s.", max_timestamp)
         complete_tracks = user.get_recent_tracks(
             limit=None, time_from=max_timestamp
         )
@@ -68,30 +70,30 @@ def main():
 
     timeline = {}
     for index_timeline, new_track in enumerate(complete_tracks, 1):
-        dict_track = {}
-        dict_track["Artist"] = new_track.track.artist
-        dict_track["Album"] = new_track.album
-        dict_track["Title"] = new_track.track.title
-        dict_track["Date"] = new_track.playback_date
-        dict_track["Timestamp"] = new_track.timestamp
+        dict_track = {
+            "Artist": new_track.track.artist,
+            "Album": new_track.album,
+            "Title": new_track.track.title,
+            "Date": new_track.playback_date,
+            "Timestamp": new_track.timestamp,
+        }
         timeline[index_timeline] = dict_track
         index_timeline += 1
 
     Path("Exports").mkdir(parents=True, exist_ok=True)
 
-    logger.debug("Creating dataframe")
+    logger.info("Creating final dataframe")
     df = pd.DataFrame.from_dict(timeline, orient="index")
     if args.file:
         df = pd.concat([df, df_initial], ignore_index=True, sort=True)
 
     max_timestamp = df["Timestamp"].astype(int).max()
 
-    logger.debug("Exporting dataframe")
-    df.to_csv(
+    export_filename = (
         f"Exports/complete_timeline_{args.username}_{max_timestamp}.txt",
-        sep="\t",
-        index=False,
     )
+    logger.info("Exporting dataframe to %s.", export_filename)
+    df.to_csv(export_filename, sep="\t", index=False)
 
     logger.info("Runtime : %.2f seconds" % (time.time() - temps_debut))
 
