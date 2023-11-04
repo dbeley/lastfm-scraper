b4b3821269fb4904217ca5a890367acbbec8cd82
dbeley <6568955+dbeley@users.noreply.github.com>
2023-11-04 01:35:28 +0100

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,18 @@
+with import <nixpkgs> { };
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.python3
+    python3Packages.pip
+
+    python3Packages.requests
+    python3Packages.pylast
+    python3Packages.pandas
+    python3Packages.beautifulsoup4
+    python3Packages.lxml
+    python3Packages.tqdm
+
+    pre-commit
+  ];
+
+}
