df23e52799bc188e84be17764d0ca4e7434d45dd
diegantobass <diego.antolinosbasso@sciencespo.fr>
2025-05-06 11:56:10 +0200

    clean code + comment

--- build_network.py
+++ build_network.py
@@ -3,25 +3,32 @@ import csv
 import networkx as nx
 from collections import defaultdict
 
+# default folder for similar artists data
 folder = "Exports/"
 
+# create graph object and nodes/edges lists
 network = nx.Graph()
-
 nodes = {}
 edges = defaultdict(int)
 
+# process files exported from the similar artists script
 for file in os.listdir(folder):
     name = file.split("_")[0]
     nodes[name] = ("", "")
-
     similars = csv.reader(open(folder + file, 'r'))
     for similar in similars:
         genres = similar[2].split(",")
         nodes[similar[0]] = (similar[1], genres)
         edges[(name, similar[0])] += 1
 
-network.add_nodes_from(nodes)
-network.add_edges_from(edges)
-print(network.number_of_nodes())
-print(network.number_of_edges())
+# add nodes and edges to the networkx graph object
+for node in nodes:
+    network.add_node(node, size=nodes[node][0], genres=nodes[node][1])
+for edge in edges:
+    network.add_edge(edge, weight=edges[edge])
+
+# print basic informations and write graph to .gexf file
+print("The output network has :")
+print("\t" + str(network.number_of_nodes()) + " nodes")
+print("\t" + str(network.number_of_edges()) + " edges")
 nx.write_gexf(network, "network.gexf")
