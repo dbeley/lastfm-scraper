8ef317df2d830bfee0fba3cef665141384fa7be0
diegantobass <diego.antolinosbasso@sciencespo.fr>
2025-05-06 11:25:27 +0200

    add networkx to produce a graph file

--- build_network.py
+++ build_network.py
@@ -1,13 +1,27 @@
 import os
 import csv
+import networkx as nx
+from collections import defaultdict
 
 folder = "Exports/"
 
-network = csv.writer(open("network.csv", 'w'))
+network = nx.Graph()
+
+nodes = {}
+edges = defaultdict(int)
 
 for file in os.listdir(folder):
     name = file.split("_")[0]
-    similars = open(folder + file).readlines()
-    similars = ','.join([x.strip() for x in similars])
-    newline = [name, similars]
-    network.writerow(newline)
+    nodes[name] = ("", "")
+
+    similars = csv.reader(open(folder + file, 'r'))
+    for similar in similars:
+        genres = similar[2].split(",")
+        nodes[similar[0]] = (similar[1], genres)
+        edges[(name, similar[0])] += 1
+
+network.add_nodes_from(nodes)
+network.add_edges_from(edges)
+print(network.number_of_nodes())
+print(network.number_of_edges())
+nx.write_gexf(network, "network.gexf")
